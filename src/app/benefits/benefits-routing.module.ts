f007ab92d374539d7b6789a57ff4a2edb06982aa
MrCandle <rodrigofvela@gmail.com>
2018-09-20 16:04:28 -0300

    core module wip

--- src/app/benefits/benefits-routing.module.ts
+++ src/app/benefits/benefits-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BenefitListComponent } from './benefit-list/benefit-list.component';
+import { BenefitDetailComponent } from './benefit-detail/benefit-detail.component';
+import { BenefitEditComponent } from './benefit-edit/benefit-edit.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BenefitListComponent
+  },
+  {
+    path: ':id',
+    component: BenefitDetailComponent
+  },
+  {
+    path: ':id/edit',
+    component: BenefitEditComponent
   }
 ];
 
